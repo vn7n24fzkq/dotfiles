e8c7f70531e05462619de637c266f70807c07733
vn7n24fzkq <vn7n24fzkq@gmail.com>
2018-09-13 17:54:52 +0800

    Update readme

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/bin/sh -e
 cd
-git clone https://github.com/vn7n24fzkq/my-linux-config.git
+
+#language
+sudo locale-gen en_US.UTF-8
+
 #install What I need 
 sudo apt-get update
 sudo apt-get -y install  curl git tmux vim 
 sudo apt-get -y install  python-pip python-dev build-essential
 sudo apt-get -y install  build-essential cmake
-sudo apt-get -y install  build-essential cmake3
 sudo apt-get -y install  python-dev python3-dev
+
+git clone https://github.com/vn7n24fzkq/my-linux-config.git
 
 #setbashrc
 cat ~/my-linux-config/config/.mybashrc >> ~/.bashrc
 
 #rust
 curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+rustup component add rust-src # for YouCompleteMe rust
 
 #java
 sudo apt-get -y install default-jdk
-sudo apt-get -y install default-jre
 
 #npm
 sudo apt-get -y install nodejs npm
@@ -27,12 +32,12 @@ ln -s -f ~/my-linux-config/config/.tmux/.tmux.conf
 ln -s -f ~/my-linux-config/config/.tmux/.tmux.conf.local 
 
 #powerline-shell
-pip install --user  powerline-shell
-ln -s -f ~/my-linux-config/config/.my-powerline-themes 
 if [! -d ~/.config ]; then
     mkdir ~/.config
 fi
-ln -s -f ~/my-linux-config/config/.config/powerline-shell ~/.config
+ln -s -f ~/my-linux-config/config/.config/powerline-shell ~/.config/powerline-shell
+pip install --user  powerline-shell
+ln -s -f ~/my-linux-config/config/.my-powerline-themes 
 
 #vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -45,5 +50,5 @@ cd ~/.vim/bundle/YouCompleteMe
 ln -s -f ~/my-linux-config/config/.gitconfig
 
 #LS_COLORS
-cp ~/my-linux-config/config/.dircolors .
+ln -s -f ~/my-linux-config/config/.dircolors 
 . $HOME/.bashrc
