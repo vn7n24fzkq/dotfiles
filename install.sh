765b83fb0ebd48df60278d63e38e895293a211c2
VN7 <vn7n24fzkq@gmail.com>
2019-01-26 22:23:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cd
 sudo locale-gen en_US.UTF-8
 
 #install What I need 
+sudo apt install man
 sudo apt-get update
 sudo apt-get -y install  curl git tmux
 sudo apt-get -y install  python-pip python-dev build-essential
@@ -31,27 +32,27 @@ sudo apt-get -y install default-jdk
 sudo apt-get -y install nodejs npm
 
 #tmux 
-ln -s -f ~/my-linux-config/config/.tmux/.tmux.conf 
-ln -s -f ~/my-linux-config/config/.tmux/.tmux.conf.local 
+cp ~/my-linux-config/config/.tmux/.tmux.conf .tmux.conf
+cp ~/my-linux-config/config/.tmux/.tmux.conf.local .tmux.conf.local
 
 #powerline-shell
 if [! -d ~/.config ]; then
     mkdir ~/.config
 fi
-ln -s -f ~/my-linux-config/config/.config/powerline-shell ~/.config/powerline-shell
+mkdir -p ~/.config && cp -R ~/my-linux-config/config/.config/powerline-shell ~/.config/powerline-shell
 pip install --user  powerline-shell
-ln -s -f ~/my-linux-config/config/.my-powerline-themes 
+cp -R ~/my-linux-config/config/.my-powerline-themes  .my-powerline-themes  
 
 #vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s -f ~/my-linux-config/config/.vimrc 
+cp ~/my-linux-config/config/.vimrc .vimrc 
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --all
 
 #git
-ln -s -f ~/my-linux-config/config/.gitconfig
+cp ~/my-linux-config/config/.gitconfig .gitconfig
 
 #LS_COLORS
-ln -s -f ~/my-linux-config/config/.dircolors 
+cp ~/my-linux-config/config/.dircolors .dircolors
 . $HOME/.bashrc
